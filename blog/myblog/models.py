cbe85da6d45770471194464c305063afc1e51deb
Edwin Kimaita <edwinkimaita@MacBook-Pro-6.local>
2018-12-20 14:46:51 +0300

    [Feature #162781892] code comment and refactor

--- blog/myblog/models.py
+++ blog/myblog/models.py
@@ -4,10 +4,8 @@ from django.urls import reverse
 
 from rest_framework.reverse import reverse as api_reverse
 # Create your models here.
-
-
-
 class BlogPost(models.Model):
+    """This class represents a blog model"""
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE)
     title = models.CharField(max_length=120, null=True, blank=True)
@@ -15,12 +13,15 @@ class BlogPost(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
+        """A representation of the blog instance"""
         return str(self.user)
 
     @property
     def owner(self):
+        """Returns the user who posted the blog"""
         return self.user
 
     def get_api_url(self, request=None):
+        """Returns the url of a specific blog"""
         return api_reverse("api-postings:post-rud",
                            kwargs={'pk': self.pk}, request=request)
